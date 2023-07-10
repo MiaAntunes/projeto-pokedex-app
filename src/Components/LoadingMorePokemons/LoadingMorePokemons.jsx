58ca98ab1b5529f016d826320524663c7b8b27a2
bandreid01 <viniciusdasilvax@gmail.com>
2023-07-10 13:58:39 -0300

    Adição carregar mais pokemons

--- src/Components/LoadingMorePokemons/LoadingMorePokemons.jsx
+++ src/Components/LoadingMorePokemons/LoadingMorePokemons.jsx
@@ -1,9 +1,30 @@
+import axios from "axios"
+import { useContext } from "react"
+import { globalContext } from "../../Context/globalContext"
 
-export const LoadingMorePokemons = ({more}) => {
-    
-    console.log(more)
+export const LoadingMorePokemons = ({ more }) => {
+
+    const { setPokemons, pokemons } = useContext(globalContext)
+
+    const loadingMorePokemon = async (more) => {
+        try {
+            const res = await axios
+                .get(more)
+            setPokemons(res.data)
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+
+    console.log(pokemons)
 
     return (
-        <></>
+        <>
+            <button
+            onClick={()=> 
+                {loadingMorePokemon(more)}
+            }
+            >Next</button>
+        </>
     )
 }
