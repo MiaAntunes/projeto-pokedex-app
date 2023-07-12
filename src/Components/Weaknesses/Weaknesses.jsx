740e5a83a6f20e637ec83196028d54b28db24a3a
bandreid01 <viniciusdasilvax@gmail.com>
2023-07-12 15:08:03 -0300

    Alteração Pokemons Fraquezas

--- src/Components/Weaknesses/Weaknesses.jsx
+++ src/Components/Weaknesses/Weaknesses.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { getPokemonType } from "../../Utils/getPokemonType";
 import { useParams } from "react-router-dom";
-import { warning } from "framer-motion";
 
-export const Weaknesses = ({ type }) => {
+
+export const Weaknesses = ({ type, condition }) => {
   const [weak, setWeak] = useState({});
-  
-  const {name} = useParams()
+
+  const { name } = useParams()
 
   useEffect(() => {
     loadingWeak(type);
@@ -21,29 +21,29 @@ export const Weaknesses = ({ type }) => {
       console.log(error.response);
     }
   };
-  
+
+  const returnData = (condition) => {
+    if (condition === 'weak') {
+      const res = weak.double_damage_from && weak.double_damage_from.map((damage, index) => {
+        return <img key={index} src={getPokemonType(damage.name)} />
+      })
+      return res
+    } else if (condition === 'strong') {
+      const res = weak.double_damage_from && weak.double_damage_to.map((damage, index) => {
+        return <img key={index} src={getPokemonType(damage.name)} />
+      })
+      return res
+    } else if (condition === 'immune') {
+      const res = weak.double_damage_from && weak.no_damage_from.map((damage, index) => {
+        return <img key={index} src={getPokemonType(damage.name)} />
+      })
+      return res
+  } 
+}
 
   return (
     <>
-      {weak.double_damage_from && <>
-        <h3>Fraco</h3>
-        {weak.double_damage_from.map((damage, index) => {
-          return <img key={index} src={getPokemonType(damage.name)} />;
-        })}
-        <h3>Forte</h3>
-        {weak.double_damage_to.map((damage, index)=>{
-          return <img key={index} src={getPokemonType(damage.name)}/>
-        })
-        }
-        <h3>Inume</h3> 
-        {weak.no_damage_from.length !== 0?
-        weak.no_damage_from.map((damage, index)=>{
-          return <img key={index} src={getPokemonType(damage.name)}/>
-        })
-        : <></>
-        }
-      </>
-      }
+        {returnData(condition)}
     </>
   );
 };
